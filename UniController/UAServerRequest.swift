9e8c4a8cb83f203b38ae740f6fc244f74e6c8272
mikomokaru-jpn <temp_iiii@yahoo.co.jp>
2021-11-11 20:22:24 +0900

    To be able to send  request of  POST JSON data

--- UniController/UAServerRequest.swift
+++ UniController/UAServerRequest.swift
@@ -4,10 +4,11 @@
 import Cocoa
 class UAServerRequest: NSObject {
     //HTTPリクエストの送受信を同期的に行う
-    //param: x-www-form-urlencoded形式のも文字列
-    //戻り値: レスポンスのJSON形式データをJSONSerializationによりJSONオブジェクトに変換して返す。
     class func postSync(urlString:String, param:String)->Any
     {
+        //param: x-www-form-urlencoded形式の文字列
+        //戻り値: レスポンスのJSON形式データをJSONSerializationによりJSONオブジェクトに変換して返す。
+        
         var list:Any = []
         //パラメータをDataオブジェクトに変換
         guard let data = param.data(using: .utf8)  else {
@@ -56,7 +57,75 @@ class UAServerRequest: NSObject {
                     let responseList = try JSONSerialization.jsonObject(with: data)
                     list = responseList
                 }catch{
-                    print("JSONSerialization fatal error")
+                    print("JSONSerialization.jsonObject fatal error")
+                    semaphore.signal()
+                    return
+                }
+                semaphore.signal()//処理の再開
+                return
+        })
+        task.resume() //実行
+        semaphore.wait() //送受信が終了するまで待機する
+        return list
+    }
+    
+    class func postSyncJ(urlString:String, param:[String:Any])->Any
+    {
+        //param: POSTパラメータを設定したディクショナリ
+        //戻り値: レスポンスのJSON形式データをJSONSerializationによりJSONオブジェクトに変換して返す。
+
+        var list:Any = []
+        //パラメータをDataオブジェクトに変換
+        let data: Data
+        do {
+            data =  try JSONSerialization.data(withJSONObject: param)
+        }catch{
+            print("JSONSerialization.data fatal error")
+            return list
+        }
+        //URLリクエストオブジェクトの作成
+        let url:URL = URL.init(string: urlString)!
+        var request:URLRequest = URLRequest(url: url)
+        //パラメータの設定
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue(String(format:"%ld", data.count) , forHTTPHeaderField: "Content-Length")
+        request.httpBody = data
+        //ネットワーク通信オブジェクトの生成（一時セッション）
+        let configuration = URLSessionConfiguration.ephemeral
+        let session = URLSession.init(configuration: configuration)
+        //同期用セマフォの作成
+        let semaphore = DispatchSemaphore(value: 0)
+        //データ送受信処理の定義
+        let task = session.dataTask(with: request,
+                                    completionHandler:
+            {(data,response,error) in
+                if let error = error{ //error=nil is OK
+                    //エラー発生
+                    print("HTTP request error occured \(error)")
+                    semaphore.signal()
+                    return
+                }
+                guard let response = response as? HTTPURLResponse,
+                    let data = data else {
+                        //guardの評価を複数条件で行う
+                        //受信データがnil または　responseがキャストできないときはエラー
+                        print("response not received")
+                        semaphore.signal()
+                        return
+                }
+                if response.statusCode != 200{
+                    //ステータスコード200(OK)以外はエラーとする
+                    print("status error \(response.statusCode)")
+                    semaphore.signal()
+                    return
+                }
+                do{ //JSON文字列をJSONオブジェクトに変換する
+                    let responseList = try JSONSerialization.jsonObject(with: data)
+                    list = responseList
+                }catch{
+                    print("JSONSerialization.jsonObject fatal error")
                     semaphore.signal()
                     return
                 }
